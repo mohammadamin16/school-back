e613f36b4758ee5a2d1dfc8b2d1a6afa7e826701
Amin <toutounchi.ma@gmail.com>
2020-08-03 23:27:26 +0430

    add migration command in Procfile

--- back/settings.py
+++ back/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'STATIC_ROOT')
 AUTH_USER_MODEL = 'accounts.User'
 
 CORS_ORIGIN_ALLOW_ALL=True
+# Activate Django-Heroku.
+django_heroku.settings(locals())
