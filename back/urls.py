0012bc23eaa231350de3cc32646f1b0494c6ed0b
Amin <toutounchi.ma@gmail.com>
2020-08-03 23:17:53 +0430

    add staticfiles to back/urls.py

--- back/urls.py
+++ back/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import views
 from study import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('study/', include('study.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
